1c9cd79c38ae7110874f84469d09a90fbf9b9733
Emil Nilsson <emilwald94@gmail.com>
2021-09-10 20:46:21 +0200

    Add project files.

---
+++ GetFilesFromFolder/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace GetFilesFromFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.lstFiles = new System.Windows.Forms.ListBox();
+            this.cmbExtn = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Location = new System.Drawing.Point(28, 51);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(354, 31);
+            this.txtDirectory.TabIndex = 2;
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.FormattingEnabled = true;
+            this.lstFiles.ItemHeight = 25;
+            this.lstFiles.Location = new System.Drawing.Point(28, 120);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(638, 254);
+            this.lstFiles.TabIndex = 3;
+            // 
+            // cmbExtn
+            // 
+            this.cmbExtn.FormattingEnabled = true;
+            this.cmbExtn.Items.AddRange(new object[] {
+            "xml",
+            "csv",
+            "html",
+            "*"});
+            this.cmbExtn.Location = new System.Drawing.Point(541, 48);
+            this.cmbExtn.Name = "cmbExtn";
+            this.cmbExtn.Size = new System.Drawing.Size(125, 33);
+            this.cmbExtn.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 450);
+            this.Controls.Add(this.cmbExtn);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.txtDirectory);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.ListBox lstFiles;
+        private System.Windows.Forms.ComboBox cmbExtn;
+    }
+}
+
---
+++ GetFilesFromFolder/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetFilesFromFolder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            dlg.ShowDialog();
+            txtDirectory.Text = dlg.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           var files = System.IO.Directory.GetFiles(txtDirectory.Text, 
+                "*."+cmbExtn.Text, System.IO.SearchOption.AllDirectories);
+            //*.* For all types
+            //System.IO.SearchOption.AllDirectories - for all folders including sub folder in the folder whose path is mentioned in the textbox
+
+            lstFiles.DataSource = files;
+
+            MessageBox.Show(files.Count() + "files found");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
